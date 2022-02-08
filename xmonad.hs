b98949f7b3f470805f4c649eb3426a7e1d3a0934
SlimTim10 <slimtim10@gmail.com>
2022-02-08 12:33:27 -0500

    XMonad smart borders

--- xmonad.hs
+++ xmonad.hs
@@ -1,11 +1,13 @@
 import XMonad
 import XMonad.Actions.GridSelect
+import XMonad.Layout.NoBorders
 import qualified Data.Map as M
 
 main :: IO ()
 main = xmonad $ def
   { modMask = mod4Mask
   , keys = \c -> mykeys c `M.union` keys defaultConfig c
+  , layoutHook = smartBorders $ layoutHook def
   }
   where
     mykeys (XConfig {modMask = modm}) = M.fromList $
