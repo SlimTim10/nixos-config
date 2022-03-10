fe5e578ecc8ce01efc9d89fa966c75983e40c0ba
SlimTim10 <slimtim10@gmail.com>
2022-03-10 15:19:23 -0500

    Qualified imports

--- xmonad.hs
+++ xmonad.hs
@@ -1,45 +1,46 @@
-import XMonad
-import XMonad.Actions.GridSelect
-import XMonad.Layout.NoBorders
-import XMonad.Hooks.DynamicLog
-import XMonad.Actions.WindowGo
-import XMonad.Prompt
-import XMonad.Prompt.Window
+import qualified XMonad as X
+import XMonad ((.|.))
+import qualified XMonad.Prompt as Prompt
+import qualified XMonad.Prompt.Window as PromptW
+import qualified XMonad.Layout.NoBorders as Layout
+import qualified XMonad.Hooks.DynamicLog as DynamicLog
+import qualified XMonad.Actions.WindowGo as WindowGo
+import XMonad.Actions.WindowGo ((=?))
 import qualified XMonad.StackSet as W
-import Data.List (isInfixOf)
-import Data.Char (toLower)
+import qualified Data.List as L
+import qualified Data.Char as C
 import qualified Data.Map as M
 
 main :: IO ()
-main = xmonad =<< xmobar def
-  { modMask = mod4Mask
-  , keys = \c -> mykeys c `M.union` keys defaultConfig c
-  , layoutHook = smartBorders $ layoutHook def
-  , borderWidth = 2
-  , focusedBorderColor = "#00FF00"
+main = X.xmonad =<< DynamicLog.xmobar X.def
+  { X.modMask = X.mod4Mask
+  , X.keys = \c -> mykeys c `M.union` X.keys X.defaultConfig c
+  , X.layoutHook = Layout.smartBorders $ X.layoutHook X.def
+  , X.borderWidth = 2
+  , X.focusedBorderColor = "#00FF00"
   }
   where
-    mykeys (XConfig {modMask = modm}) = M.fromList $
-      [ ((modm, xK_x), spawn "lock-screen")
+    mykeys (X.XConfig {X.modMask = modm}) = M.fromList $
+      [ ((modm, X.xK_x), X.spawn "lock-screen")
       -- Go to window by name search
-      , ((modm, xK_s), windowPrompt
-          def
-          { searchPredicate = myFuzzyFinderFunction
-          , alwaysHighlight = True
+      , ((modm, X.xK_s), PromptW.windowPrompt
+          X.def
+          { Prompt.searchPredicate = myFuzzyFinderFunction
+          , Prompt.alwaysHighlight = True
           }
-          Goto
-          allWindows)
+          PromptW.Goto
+          PromptW.allWindows)
       -- Bring window by name search
-      , ((modm .|. shiftMask, xK_s), windowPrompt
-          def { searchPredicate = myFuzzyFinderFunction }
-          Bring
-          allWindows)
+      , ((modm .|. X.shiftMask, X.xK_s), PromptW.windowPrompt
+          X.def { Prompt.searchPredicate = myFuzzyFinderFunction }
+          PromptW.Bring
+          PromptW.allWindows)
       -- Go to emacs
-      , ((modm, xK_e), runOrRaise "emacs" (className =? "Emacs"))
+      , ((modm, X.xK_e), WindowGo.runOrRaise "emacs" (WindowGo.className =? "Emacs"))
       -- Movement bindings like emacs
-      , ((modm, xK_o), windows W.focusDown)
-      , ((modm .|. shiftMask, xK_o), windows W.focusUp)
+      , ((modm, X.xK_o), X.windows W.focusDown)
+      , ((modm .|. X.shiftMask, X.xK_o), X.windows W.focusUp)
       ]
 
 myFuzzyFinderFunction :: String -> String -> Bool
-myFuzzyFinderFunction a b = map toLower a `isInfixOf` map toLower b
+myFuzzyFinderFunction a b = map C.toLower a `L.isInfixOf` map C.toLower b
