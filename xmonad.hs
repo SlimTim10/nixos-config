ea3d1274ec1f0e26f56f93d5ade68f26d595c43a
SlimTim10 <slimtim10@gmail.com>
2022-03-10 15:50:17 -0500

    Use EZConfig keys

--- xmonad.hs
+++ xmonad.hs
@@ -7,40 +7,108 @@ import qualified XMonad.Hooks.DynamicLog as DynamicLog
 import qualified XMonad.Actions.WindowGo as WindowGo
 import XMonad.Actions.WindowGo ((=?))
 import qualified XMonad.StackSet as W
+import XMonad.Util.EZConfig as EZConfig
 import qualified Data.List as L
 import qualified Data.Char as C
 import qualified Data.Map as M
 
 main :: IO ()
-main = X.xmonad =<< DynamicLog.xmobar X.def
-  { X.modMask = X.mod4Mask
-  , X.keys = \c -> mykeys c `M.union` X.keys X.defaultConfig c
-  , X.layoutHook = Layout.smartBorders $ X.layoutHook X.def
-  , X.borderWidth = 2
-  , X.focusedBorderColor = "#00FF00"
-  }
-  where
-    mykeys (X.XConfig {X.modMask = modm}) = M.fromList $
-      [ ((modm, X.xK_x), X.spawn "lock-screen")
-      -- Go to window by name search
-      , ((modm, X.xK_s), PromptW.windowPrompt
-          X.def
-          { Prompt.searchPredicate = myFuzzyFinderFunction
-          , Prompt.alwaysHighlight = True
-          }
-          PromptW.Goto
-          PromptW.allWindows)
-      -- Bring window by name search
-      , ((modm .|. X.shiftMask, X.xK_s), PromptW.windowPrompt
-          X.def { Prompt.searchPredicate = myFuzzyFinderFunction }
-          PromptW.Bring
-          PromptW.allWindows)
-      -- Go to emacs
-      , ((modm, X.xK_e), WindowGo.runOrRaise "emacs" (WindowGo.className =? "Emacs"))
-      -- Movement bindings like emacs
-      , ((modm, X.xK_o), X.windows W.focusDown)
-      , ((modm .|. X.shiftMask, X.xK_o), X.windows W.focusUp)
-      ]
+main = do
+  xmobar <- DynamicLog.xmobar $ X.def
+    { X.modMask = X.mod4Mask
+    -- , X.keys = \c -> mykeys c `M.union` X.keys X.defaultConfig c
+    , X.layoutHook = Layout.smartBorders $ X.layoutHook X.def
+    , X.borderWidth = 2
+    , X.focusedBorderColor = "#00FF00"
+    }
+    `EZConfig.additionalKeysP`
+    [ ("M-x", X.spawn "lock-screen")
+    -- Go to window by name search
+    , ("M-s", PromptW.windowPrompt
+        X.def
+        { Prompt.searchPredicate = myFuzzyFinderFunction
+        , Prompt.alwaysHighlight = True
+        }
+        PromptW.Goto
+        PromptW.allWindows)
+    -- Bring window by name search
+    , ("M-S-s", PromptW.windowPrompt
+        X.def { Prompt.searchPredicate = myFuzzyFinderFunction }
+        PromptW.Bring
+        PromptW.allWindows)
+    -- Go to emacs
+    , ("M-e", WindowGo.runOrRaise "emacs" (WindowGo.className =? "Emacs"))
+    -- Movement bindings like emacs
+    , ("M-o", X.windows W.focusDown)
+    , ("M-S-o", X.windows W.focusUp)
+    ]
+  X.xmonad xmobar
 
 myFuzzyFinderFunction :: String -> String -> Bool
 myFuzzyFinderFunction a b = map C.toLower a `L.isInfixOf` map C.toLower b
+
+{-
+DEFAULT KEY BINDINGS
+FROM: https://hackage.haskell.org/package/xmonad-0.17.0/docs/src/XMonad.Config.html
+
+keys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
+keys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
+    -- launching and killing programs
+    [ ((modMask .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf) -- %! Launch terminal
+    , ((modMask,               xK_p     ), spawn "dmenu_run") -- %! Launch dmenu
+    , ((modMask .|. shiftMask, xK_p     ), spawn "gmrun") -- %! Launch gmrun
+    , ((modMask .|. shiftMask, xK_c     ), kill) -- %! Close the focused window
+
+    , ((modMask,               xK_space ), sendMessage NextLayout) -- %! Rotate through the available layout algorithms
+    , ((modMask .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf) -- %!  Reset the layouts on the current workspace to default
+
+    , ((modMask,               xK_n     ), refresh) -- %! Resize viewed windows to the correct size
+
+    -- move focus up or down the window stack
+    , ((modMask,               xK_Tab   ), windows W.focusDown) -- %! Move focus to the next window
+    , ((modMask .|. shiftMask, xK_Tab   ), windows W.focusUp  ) -- %! Move focus to the previous window
+    , ((modMask,               xK_j     ), windows W.focusDown) -- %! Move focus to the next window
+    , ((modMask,               xK_k     ), windows W.focusUp  ) -- %! Move focus to the previous window
+    , ((modMask,               xK_m     ), windows W.focusMaster  ) -- %! Move focus to the master window
+
+    -- modifying the window order
+    , ((modMask,               xK_Return), windows W.swapMaster) -- %! Swap the focused window and the master window
+    , ((modMask .|. shiftMask, xK_j     ), windows W.swapDown  ) -- %! Swap the focused window with the next window
+    , ((modMask .|. shiftMask, xK_k     ), windows W.swapUp    ) -- %! Swap the focused window with the previous window
+
+    -- resizing the master/slave ratio
+    , ((modMask,               xK_h     ), sendMessage Shrink) -- %! Shrink the master area
+    , ((modMask,               xK_l     ), sendMessage Expand) -- %! Expand the master area
+
+    -- floating layer support
+    , ((modMask,               xK_t     ), withFocused $ windows . W.sink) -- %! Push window back into tiling
+
+    -- increase or decrease number of windows in the master area
+    , ((modMask              , xK_comma ), sendMessage (IncMasterN 1)) -- %! Increment the number of windows in the master area
+    , ((modMask              , xK_period), sendMessage (IncMasterN (-1))) -- %! Deincrement the number of windows in the master area
+
+    -- quit, or restart
+    , ((modMask .|. shiftMask, xK_q     ), io (exitWith ExitSuccess)) -- %! Quit xmonad
+    , ((modMask              , xK_q     ), spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi") -- %! Restart xmonad
+
+    , ((modMask .|. shiftMask, xK_slash ), helpCommand) -- %! Run xmessage with a summary of the default keybindings (useful for beginners)
+    -- repeat the binding for non-American layout keyboards
+    , ((modMask              , xK_question), helpCommand) -- %! Run xmessage with a summary of the default keybindings (useful for beginners)
+    ]
+    ++
+    -- mod-[1..9] %! Switch to workspace N
+    -- mod-shift-[1..9] %! Move client to workspace N
+    [((m .|. modMask, k), windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+    ++
+    -- mod-{w,e,r} %! Switch to physical/Xinerama screens 1, 2, or 3
+    -- mod-shift-{w,e,r} %! Move client to screen 1, 2, or 3
+    [((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+
+
+FROM SOMEWHERE ELSE?
+,((modm, xK_b     ), sendMessage ToggleStruts)
+-}
