380e788540af289a372c63ad3229d70f8a12e6e5
SlimTim10 <slimtim10@gmail.com>
2023-01-13 13:26:37 -0500

    Configure PostgreSQL peer authentication

--- configuration.nix
+++ configuration.nix
@@ -138,6 +138,7 @@ in
     enableTCPIP = true;
     authentication = pkgs.lib.mkOverride 14 ''
       local all all trust
+      host all all 127.0.0.1/32 trust
       host all all ::1/128 trust
     '';
   };
