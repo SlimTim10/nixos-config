7490d056d0840b28e4f15c133b2678deeb8afddf
SlimTim10 <slimtim10@gmail.com>
2023-04-03 14:08:36 -0400

    Update xmonad setting for new nix version

--- xmonad.nix
+++ xmonad.nix
@@ -7,4 +7,6 @@
   };
   services.xserver.autorun = true;
   services.xserver.displayManager.defaultSession = "none+xmonad";
+
+  services.xserver.windowManager.xmonad.config = builtins.readFile /home/tim/.nix-config/xmonad.hs;
 }
