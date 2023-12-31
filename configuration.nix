f2da38ab79b56a8cfc8775f1876b15f06b5c400b
SlimTim10 <slimtim10@gmail.com>
2023-12-31 11:30:13 -0500

    Add nix-community cache

--- configuration.nix
+++ configuration.nix
@@ -10,10 +10,11 @@
 
   nixpkgs.config.allowUnfree = lib.mkForce true;
 
-  # Use the systemd-boot EFI boot loader.
+  # Use the systemd-boot EFI boot loader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.useOSProber = true;
+  # Limit the number of generations to keep
   boot.loader.grub.configurationLimit = 4;
 
   # Networking
@@ -88,11 +89,12 @@
 
     substituters = [
       "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
     ];
 
     trusted-public-keys = [
-      # the default public key of cache.nixos.org, it's built-in, no need to add it here
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
