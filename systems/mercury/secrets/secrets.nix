bb33039f6fd03b3a092f62f0b490f8cf7d7b426e
tim <slimtim10@gmail.com>
2025-04-02 15:07:02 -0400

    New machine

--- systems/mercury/secrets/secrets.nix
+++ systems/mercury/secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  mercury = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPr6JeuJ0hWTNYqhCAcIjowl2+M5S67vez9MFTVOT56Z slimtim10@gmail.com";
+  mercury = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICWzyr6jDbd7VMyTLOVeAazr9OAbrDKTVEBbXnlYqUfq slimtim10@gmail.com";
 in {
   "syncthingApiKey.age".publicKeys = [ mercury ];
 }
