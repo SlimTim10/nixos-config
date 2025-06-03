1157327fd0739c8d04d369a5bf4e20e9cdac0394
SlimTim10 <slimtim10@gmail.com>
2025-06-03 11:34:19 -0400

    Setting up new laptop

--- systems/mercury/hardware-configuration.nix
+++ systems/mercury/hardware-configuration.nix
@@ -8,36 +8,39 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "thunderbolt" "rtsx_pci_sdmmc" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/c94a27d4-b59d-4a23-9724-c775a43ea70b";
+    { device = "/dev/disk/by-uuid/192de655-96f8-4fb4-b151-8482676db184";
       fsType = "ext4";
     };
 
-  boot.initrd.luks.devices."nixos".device = "/dev/disk/by-uuid/ec40a388-a8ec-448c-8b9b-c4692ac125e5";
-
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/197C-28BD";
+    { device = "/dev/disk/by-uuid/0BF8-CA33";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/f8374e2b-df1a-46a7-ad54-182d3cf8e7ca"; }
-    ];
+  swapDevices = [ ];
+
+  boot.initrd.luks.devices = {
+    crypted = {
+      device = "/dev/disk/by-uuid/b399909f-1b9a-4edf-9bdb-06a1feac213c";
+      preLVM = true;
+    };
+  };
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
+
--- systems/mercury/secrets/secrets.nix
+++ systems/mercury/secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  mercury = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPr6JeuJ0hWTNYqhCAcIjowl2+M5S67vez9MFTVOT56Z slimtim10@gmail.com";
+  mercury = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIt3wtB4p/Zw9MkrEEFxpHJ+TE1n2cy+fdu+WIyCfVXd slimtim10@gmail.com";
 in {
   "syncthingApiKey.age".publicKeys = [ mercury ];
 }
